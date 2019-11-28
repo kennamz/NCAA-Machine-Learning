f429746a1f0d17ab4e2f50ff555d680f85f54f80
kennamz <kenna@zimfam.org>
2019-11-27 19:00:50 -0700

    added code to generate TeamIDs for each year listed in NCAATourneySeeds.csv

--- read_in_data.py
+++ read_in_data.py
@@ -2,10 +2,11 @@ import csv
 import os
 import numpy as np
 from numpy import linalg as LA
+from itertools import combinations 
 
 def read_in_b_data():
     """
-    Returns a vector b.
+    Returns a numpy matrix b.
     Column 1 of b contains the ID, a 14-character string of the
         format SSSS_XXXX_YYYY, where SSSS is the four digit season number,
         XXXX is the four-digit TeamID of the lower-ID team, and YYYY is the
@@ -14,19 +15,53 @@ def read_in_b_data():
         -1 if higher ID won.
     Reads data from NCAATourneySeeds.csv and NCAATourneyCompactResults.csv
     """
-    seed_file = open(os.getcwd() + "/data/Stage2DataFiles/NCAATourneySeedsShort.csv")
+    ########
+    # Get the TeamID for each year for each team to make the first column of b
+    ########
+    # open seed file
+    seed_file = open(os.getcwd() + "/data/Stage2DataFiles/NCAATourneySeeds.csv")
     seed_reader = csv.reader(seed_file, delimiter = ',')
+    # get first year
     next(seed_reader) # first line is header
-    for row in seed_reader:
-        print(row)
-        ID = row[0] + "_" # puts year into ID string
+    first_year = next(seed_reader)[0]
+    seed_file.seek(0)
+    next(seed_reader) # first line is header
     
+    prev_year = first_year
+    IDs = []
+    teams = []
+    for row in seed_reader:
+        if (row[0] == prev_year):
+            teams.append(row[2])
+        else: 
+            combos = list(combinations(teams, 2))
+            for team_pair in combos:
+                if int(team_pair[0]) < int(team_pair[1]):
+                    ID = prev_year + "_" + team_pair[0] + "_" + team_pair[1]
+                else:
+                    ID = prev_year + "_" + team_pair[1] + "_" + team_pair[0]
+                IDs.append(ID)
+            teams = []
+            combos = []
+            teams.append(row[2])
+        prev_year = row[0]
+    combos = list(combinations(teams, 2))
+    for team_pair in combos:
+        if int(team_pair[0]) < int(team_pair[1]):
+            ID = prev_year + "_" + team_pair[0] + "_" + team_pair[1]
+        else:
+            ID = prev_year + "_" + team_pair[1] + "_" + team_pair[0]
+        IDs.append(ID)
+    
+    ########
+    # Get whether each team won (1), lost (-1), or did not play (0)
+    ########
     #results_file = open(os.getcwd() +"/data/Stage2DataFiles/NCAATourneyCompactResults.csv")
     #results_reader = csv.reader(results_file, delimiter = ',')
 
 
         
-    #return [np.matrix()]
+    #return [np.matrix(IDs)]
 
 '''
 def read_in_A_data():
